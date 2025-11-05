28682fd895f9c6c8054ddb2a4afb758d8a73352e
DivanadiumPentaoxide <186297309+DivanadiumPentaoxide@users.noreply.github.com>
2025-11-05 21:58:49 +0800

    feat: upload resource code

---
+++ main.spec
@@ -0,0 +1,112 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+EXCLUDE_DLLS = [
+    "VCRUNTIME140.dll",
+    "VCRUNTIME140_1.dll",
+    "ucrtbase.dll",
+    "MSVCP140.dll",
+]
+EXCLUDE_DLL_PREFIX = ("api-ms-win-crt-", "api-ms-win-core-")
+
+
+def _fix_binaries(binaries):
+    return [
+        binary
+        for binary in binaries
+        if not binary[0] in EXCLUDE_DLLS
+        and not binary[0].startswith(EXCLUDE_DLL_PREFIX)
+    ]
+
+
+a = Analysis(
+    ["src/main.py"],
+    pathex=["."],
+    datas=[],
+    hiddenimports=[],
+    hookspath=[],
+    binaries=[
+        ("./lib/InjectionLib.dll", "."),
+        ("./assets/icon.ico", "."),
+    ],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=[
+        "email",
+        "random",
+        "logging",
+        "urllib",
+        "socket",
+        "base64",
+        "selectors",
+        "textwrap",
+        "tarfile",
+        "argparse",
+        "py_compile",
+        "calendar",
+        "copy",
+        "csv",
+        "dataclasses",
+        "bz2",
+        "gettext",
+        "getopt",
+        "pickle",
+        "lzma",
+        "quopri",
+        "string",
+        "stringprep",
+        "tracemalloc",
+        "inspect",
+        "ast",
+        "token",
+        "tokenize",
+        "unicodedata",
+        "_socket",
+        "_colorize",
+        "_py_abc",
+        "_strptime",
+        # Base Libraires
+        "abc",
+        "codecs",
+        "genericpath",
+        "heapq",
+        "io",
+        "linecache",
+        "ntpath",
+        "posixpath",
+        "traceback",
+        "_collections_abc",
+        "stat",
+        "sre_compile",
+        "sre_constants",
+        "sre_parse",
+    ],
+    win_no_prefer_redirects=False,
+    win_private_assemblies=False,
+    noarchive=False,
+    optimize=2,
+)
+
+a.binaries = _fix_binaries(a.binaries)
+
+pyz = PYZ(a.pure, a.zipped_data)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    a.binaries,
+    a.zipfiles,
+    a.datas,
+    [],
+    name="Minecraft DLL 注入工具",
+    debug=False,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=True,
+    console=False,
+    icon="assets/icon.ico",
+    disable_windowed_traceback=False,
+    argv_emulation=False,
+    target_arch=None,
+    codesign_identity=None,
+    entitlements_file=None,
+)
---
+++ src/main.py
@@ -0,0 +1,77 @@
+import os
+import sys
+import webbrowser
+import tkinter as tk
+from tkinter import ttk, filedialog, messagebox
+import injector
+
+
+def try_launch_minecraft() -> int:
+    webbrowser.open("minecraft:")
+    return injector.get_process_id("Minecraft.Windows.exe")
+
+
+class MainWindow(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Minecraft DLL 注入工具")
+        self.resizable(False, False)
+        if getattr(sys, "frozen", False):
+            icon = os.path.join(os.path.dirname(os.path.abspath(__file__)), "icon.ico")
+        else:
+            icon = os.path.join(
+                os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+                "assets/icon.ico",
+            )
+        self.iconbitmap(icon)
+
+        self.process_name = tk.StringVar()
+        self.dll_path = tk.StringVar()
+
+        pad = {"padx": 5, "pady": 5, "sticky": "ew"}
+
+        ttk.Label(self, text="请选择需要注入的 DLL 文件：").grid(row=1, column=0, **pad)
+        ttk.Entry(self, textvariable=self.dll_path, state="readonly", width=30).grid(
+            row=1, column=1, **pad
+        )
+        ttk.Button(self, text="浏览…", command=self.browse_dll).grid(
+            row=1, column=2, **pad
+        )
+
+        ttk.Button(self, text="注入", command=self.on_inject).grid(
+            row=2, column=0, columnspan=3, pady=10
+        )
+
+    def browse_dll(self):
+        filetypes = [("请选择需要注入的 DLL 文件", "*.dll"), ("所有文件", "*.*")]
+        path = filedialog.askopenfilename(title="选择要注入的 DLL", filetypes=filetypes)
+        if path:
+            self.dll_path.set(os.path.normpath(path))
+
+    def on_inject(self):
+        dll = self.dll_path.get().strip()
+        if not dll:
+            messagebox.showwarning("输入错误", "请先需要注入的 DLL 文件")
+            return
+
+        proc = injector.get_process_id("Minecraft.Windows.exe")
+        if proc == 0:
+            proc = try_launch_minecraft()
+
+        if proc == 0:
+            raise RuntimeError("无法启动 Minecraft!")
+
+        try:
+            ok = injector.inject_dll(proc, dll)
+        except Exception as e:
+            messagebox.showerror("异常", f"注入过程发生异常：\n{e}")
+            return
+
+        if ok:
+            messagebox.showinfo("成功", "DLL 注入成功！")
+        else:
+            messagebox.showerror("失败", "DLL 注入失败！")
+
+
+if __name__ == "__main__":
+    MainWindow().mainloop()
